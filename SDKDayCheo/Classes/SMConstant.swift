3b7402402861bf59cf97736bbdf1a24ad37814f7
Hintoro <hintoro@Hintoros-MacBook-Pro.local>
2019-10-17 14:12:55 +0700

    Update SMConstant.swift

--- SDKDayCheo/Classes/SMConstant.swift
+++ SDKDayCheo/Classes/SMConstant.swift
@@ -16,7 +16,7 @@ public class SMConfig: NSObject {
 //public var smbundle = SMConfig.isDebug ? "com.cloudmusic.mymp3" : Bundle.main.bundleIdentifier!
 public let kUrl = "http://cpibe.appboom.net/api/v2/get_campaign"
 public let kUrlClickAd = "http://cpibe.appboom.net/api/v2/click_ad"
-public let kUrlViewAd = "http://cpibe.appboom.net/api/v2/click_ad"
+public let kUrlViewAd = "http://cpibe.appboom.net/api/v2/view_ad"
 
 public func getBundlePath() -> Bundle {
     let pathBundle = Bundle.main.path(forResource: "SDKDayCheo", ofType: "bundle")
